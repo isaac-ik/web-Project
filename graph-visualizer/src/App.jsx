20bcf8e052c8e98c9aee54f5bb5aa7d45e1cb5d7
Isaac Ikhaiduwor <isaac.ikpemi@gmail.com>
2025-03-01 18:16:48 -0400

    graph visualizer v1.3

--- graph-visualizer/src/App.jsx
+++ graph-visualizer/src/App.jsx
@@ -38,54 +38,60 @@ const GraphVisualizer = () => {
   };
 
   return (
-    <div className="p-4">
-      <textarea
-        rows="5"
-        cols="50"
-        value={data}
-        onChange={handleInputChange}
-        placeholder="Enter data as: DataA, DataB\nExample:\nApple,Fruit\nDog,Animal"
-        className="border p-2 w-full"
-      ></textarea>
-      <button onClick={parseData} className="bg-blue-500 text-white px-4 py-2 mt-2">
-        Generate Graph
-      </button>
-      <CytoscapeComponent
-        elements={elements}
-        style={{ width: "100%", height: "500px", marginTop: "10px" }}
-        layout={{ name: "cose", nodeRepulsion: 10000, idealEdgeLength: 100 }}
-        stylesheet={[
-          {
-            selector: ".left-node",
-            style: {
-              label: "data(label)",
-              "background-color": "#ff5733",
-              color: "white",
-              "text-valign": "center",
-              "text-halign": "center",
+    <div className="p-4 flex">
+      {/* Left Section: Input and Button */}
+      <div className="w-1/3 p-4">
+        <textarea
+          rows="5"
+          cols="50"
+          value={data}
+          onChange={handleInputChange}
+          placeholder="Enter data as: DataA, DataB\nExample:\nApple,Fruit\nDog,Animal"
+          className="border p-2 w-full"
+        ></textarea>
+        <button onClick={parseData} className="bg-blue-500 text-white px-4 py-2 mt-2 w-full">
+          Generate Graph
+        </button>
+      </div>
+      {/* Right Section: Graph Canvas */}
+      <div className="w-2/3 p-4">
+        <CytoscapeComponent
+          elements={elements}
+          style={{ width: "100%", height: "500px" }}
+          layout={{ name: "cose", nodeRepulsion: 10000, idealEdgeLength: 100 }}
+          stylesheet={[
+            {
+              selector: ".left-node",
+              style: {
+                label: "data(label)",
+                "background-color": "#ff5733",
+                color: "white",
+                "text-valign": "center",
+                "text-halign": "center",
+              },
             },
-          },
-          {
-            selector: ".right-node",
-            style: {
-              label: "data(label)",
-              "background-color": "#33ff57",
-              color: "white",
-              "text-valign": "center",
-              "text-halign": "center",
+            {
+              selector: ".right-node",
+              style: {
+                label: "data(label)",
+                "background-color": "#33ff57",
+                color: "white",
+                "text-valign": "center",
+                "text-halign": "center",
+              },
             },
-          },
-          {
-            selector: "edge",
-            style: {
-              width: 2,
-              "line-color": "#ccc",
-              "target-arrow-color": "#ccc",
-              "target-arrow-shape": "triangle",
+            {
+              selector: "edge",
+              style: {
+                width: 2,
+                "line-color": "#ccc",
+                "target-arrow-color": "#ccc",
+                "target-arrow-shape": "triangle",
+              },
             },
-          },
-        ]}
-      />
+          ]}
+        />
+      </div>
     </div>
   );
 };
